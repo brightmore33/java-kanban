8ab9e893b5b4f62830365a8390180609824aa74e
Valerii Mityukhin <caleb-brightmore@yandex.ru>
2024-08-07 16:21:24 +0300

    3 простые задачи, классы Epic и Subtask
    Signed-off-by: Valerii Mityukhin <caleb-brightmore@yandex.ru>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import Models.Task;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Поехали!");
+
+        // накидываем задачи
+        Task task1 = new Task("Глажка", "Погладить кота, а лучше вещи");
+        Task task2 = new Task("Мусор", "Вынести мусор");
+        Task task3 = new Task("Собака", "Её тоже надо гладить");
     }
 }
