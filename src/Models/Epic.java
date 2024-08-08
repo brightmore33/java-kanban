9753104067101631617817df10b941a69de3a6c5
Valerii Mityukhin <caleb-brightmore@yandex.ru>
2024-08-08 10:23:00 +0300

    Приготовлен Epic надеюсь конечно...
    Signed-off-by: Valerii Mityukhin <caleb-brightmore@yandex.ru>

--- src/Models/Epic.java
+++ src/Models/Epic.java
@@ -1,4 +1,36 @@
 package Models;
 
-public class Epic {
+import Enums.TaskState;
+
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private ArrayList<Integer> subTasksIDs = new ArrayList<>();
+
+    // конструктов Эпика
+    public Epic(String name, String description) {
+        super(name, description, TaskState.NEW);
+    }
+
+    // --== Методы Эпиков ==--
+
+    // получаем идентификаторы подзадач
+    public ArrayList<Integer> getSubTasksIDs() {
+        return new ArrayList<>(subTasksIDs);
+    }
+
+    // добавление идентификатора подзадачи
+    public void addSubTaskID(Integer idSubTask) {
+        subTasksIDs.add(idSubTask);
+    }
+
+    // удаление подзадачи по идентификатору
+    public void deleteSubTaskID(Integer idSubTask) {
+        subTasksIDs.remove(idSubTask);
+    }
+
+    // очистка
+    public void clearSubTasksIDs() {
+        subTasksIDs.clear();
+    }
 }
