3f28ab39bf281b77a88f629ec57791cae3a5cfa5
Valerii Mityukhin <caleb-brightmore@yandex.ru>
2024-10-08 15:11:44 +0300

    InMemoryHistoryManager add history node
    Signed-off-by: Valerii Mityukhin <caleb-brightmore@yandex.ru>

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,13 +1,42 @@
 package manager;
 
-import models.Epic;
-import models.Subtask;
-import models.Task;
 
+import models.Task;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class InMemoryHistoryManager {
+public class InMemoryHistoryManager implements HistoryManager {
 
+    private final Map<Integer, Node<Task>> historyMap = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+    private int size = 0;
 
+    public class Node<T> {
+        public T data;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node(Node<T> prev, T data, Node<T> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(Task task) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return List.of();
+    }
 }
