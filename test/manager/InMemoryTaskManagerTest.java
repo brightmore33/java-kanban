d0eec59b2f7467a7a0e99da5ddbec79bde5638dc
Valerii Mityukhin <caleb-brightmore@yandex.ru>
2024-10-09 16:15:35 +0300

    Tests for Subtasks
    Signed-off-by: Valerii Mityukhin <caleb-brightmore@yandex.ru>

---
+++ test/manager/InMemoryTaskManagerTest.java
@@ -0,0 +1,19 @@
+package manager;
+
+import models.Epic;
+import models.Subtask;
+import models.Task;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryTaskManagerTest {
+    private Task task;
+    private Epic epic;
+    private Subtask subtask;
+    private TaskManager taskManager;
+    private HistoryManager historyManager;
+
+    @BeforeEach
+
+}
---
+++ test/models/SubtaskTest.java
@@ -0,0 +1,19 @@
+package models;
+
+import emuns.TaskState;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubtaskTest {
+    @Test
+    public void shouldReturnTrueWhenSubTasksHaveSameId() {
+        Subtask subTask1 = new Subtask(4, "Первая подзадача", "румбаааа", TaskState.NEW);
+        Subtask subTask2 = new Subtask(4, "Первая подзадача", "румбаааа", TaskState.NEW);
+
+        subTask1.setId(5);
+        subTask2.setId(5);
+
+        assertEquals(subTask1, subTask2);
+    }
+}
