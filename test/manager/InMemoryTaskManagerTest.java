9bc5ba4630149a17a487163f1ce28a9a85f171d4
Valerii Mityukhin <caleb-brightmore@yandex.ru>
2024-10-09 18:28:45 +0300

    Tests:  @BeforeEach   shouldAddNewTask   taskStabilityWhenAddToManager
    Signed-off-by: Valerii Mityukhin <caleb-brightmore@yandex.ru>

--- test/manager/InMemoryTaskManagerTest.java
+++ test/manager/InMemoryTaskManagerTest.java
@@ -1,9 +1,12 @@
 package manager;
 
+import emuns.TaskState;
 import models.Epic;
 import models.Subtask;
 import models.Task;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +18,32 @@ class InMemoryTaskManagerTest {
     private HistoryManager historyManager;
 
     @BeforeEach
+    void prepare() {
+        task = new Task("Первая задача", "Пурум пум", TaskState.NEW);
+        epic = new Epic("Первый Эпик", "Рум пум пам");
+        taskManager = Managers.getDefault();
+        historyManager = Managers.getDefaultHistory();
+    }
 
+    @Test
+    void shouldAddNewTask() {
+        Task expectedTask = new Task(0, "Первая задача", "Пурум пум",TaskState.NEW);
+
+        Task currentTask = taskManager.addNewTask(task);
+
+        Assertions.assertEquals(expectedTask, currentTask);
+        Assertions.assertNotNull(currentTask);
+        Assertions.assertNotNull(currentTask.getId());
+    }
+
+    @Test
+    public void taskStabilityWhenAddToManager() {
+        taskManager.addNewTask(task);
+
+        Task currentTask = taskManager.getTaskById(0);
+
+        Assertions.assertEquals(task.getId(), currentTask.getId());
+        Assertions.assertEquals(task.getName(), currentTask.getName());
+        Assertions.assertEquals(task.getState(), currentTask.getState());
+    }
 }
